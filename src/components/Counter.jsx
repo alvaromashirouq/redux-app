455860a974e7b9636c4aee96ef93a6f32b3a26d6
Alvaro Martinez <alvaro.martinez@unosquare.com>
2022-07-22 17:43:24 -0500

    using redux to update state

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,29 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 export const Counter = () => {
+  const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
 
   const toggleCounterHandler = () => {};
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  };
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' });
+  };
+  const incrementBy5Handler = () => {
+    dispatch({ type: 'increase', amount: 5 });
+  };
 
   return (
     <main>
       <h1>Redux Counter</h1>
-      <div>--- COUNTER VALUE</div>
+      <div>{counter}</div>
+      <div>
+        <button onClick={decrementHandler}>decrement</button>
+        <button onClick={incrementBy5Handler}>increment by 5</button>
+        <button onClick={incrementHandler}>increment</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle counter</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,42 @@
+import { createSlice } from '@reduxjs/toolkit';
 import { createStore } from 'redux';
-const counterReducer = (state = { counter: 0 }, action) => {
+
+const initialState = {
+  counter: 0,
+  showCounter: true
+};
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    togggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    }
+  }
+});
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
     return {
       counter: state.counter + 1
     };
   }
+
+  if (action.type === 'increase') {
+    return {
+      counter: state.counter + action.amount
+    };
+  }
+
   if (action.type === 'decrement') {
     return {
       counter: state.counter - 1
